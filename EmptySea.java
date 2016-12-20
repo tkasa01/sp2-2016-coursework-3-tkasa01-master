3d2ee7ce97b40cec978aeba80ddd4821f6036dca
Tanya Kasa <kasatanya1@gmail.com>
2016-12-20 00:57:48 +0000

    some methods and constructor was added

--- EmptySea.java
+++ EmptySea.java
@@ -1,7 +1,25 @@
 package battleship;
-
+import java.util.*;
 /**
  * Created by tkasa on 20/12/2016.
  */
 public class EmptySea extends Ship{
+    public EmptySea(int length) {
+        super(length);
+    }
+
+    @Override
+    public boolean shootAt(int row,int column){
+       return true;
+    }
+
+    @Override
+    public boolean isSunk(){
+        return true;
+    }
+
+    @Override
+    public String getShipType() {
+        return "Empty Sea";
+    }
 }
