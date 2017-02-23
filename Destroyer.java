af67ca8f63602ec3263c951422d06e73682d77db
tkasa01 <taniakysa@mail.ru>
2017-02-23 19:48:33 +0000

    Update Destroyer class

--- Destroyer.java
+++ Destroyer.java
@@ -3,5 +3,25 @@ import java.util.*;
 /**
  * Created by tkasa on 20/12/2016.
  */
-public class Destroyer extends Ship {
+/*3 ships for 2 places
+*
+ */
+public class Destroyer extends Ship{
+
+    /**
+     *  constructor
+     */
+    public Destroyer ()
+    {
+        super(2);
+    }
+
+    /**
+     * @return the string name of the ship
+     */
+    @Override
+    public String getShipType() {
+
+        return "destroyer" ;
+    }
 }
