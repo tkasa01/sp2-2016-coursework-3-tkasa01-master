f9892fb0e69fb456fa3f447d76e5f44602dae142
tkasa01 <taniakysa@mail.ru>
2017-02-23 19:45:58 +0000

    Update Cruiser.java

--- Cruiser.java
+++ Cruiser.java
@@ -3,5 +3,22 @@ import java.util.*;
 /**
  * Created by tkasa on 20/12/2016.
  */
-public class Cruiser  extends Ship{
+public class Cruiser extends Ship {
+    /**
+     * constructor
+     */
+    public Cruiser ()
+    {
+        super(3);
+    }
+
+    /**
+     *
+     * @return the string name of the ship
+     */
+    @Override
+    public String getShipType() {
+
+        return "cruiser" ;
+    }
 }
