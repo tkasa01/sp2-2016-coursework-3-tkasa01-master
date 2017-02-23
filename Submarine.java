7c72be60aa384786d3ffd7c49ffc72fb3997e601
tkasa01 <taniakysa@mail.ru>
2017-02-23 19:57:27 +0000

    Update Submarine.java

--- Submarine.java
+++ Submarine.java
@@ -4,4 +4,19 @@ import java.util.*;
  * Created by tkasa on 20/12/2016.
  */
 public class Submarine extends Ship{
+
+
+    public Submarine () {
+
+        super(1);
+    }
+
+    /**
+     *
+     * @return the string name of the ship
+     */
+    @Override
+    public String getShipType() {
+        return "submarine" ;
+    }
 }
